13c06ec557e6460ff2e60b4e75e0348260c5f22e
Kaushal Basnet <kaushalbasnet988@gmail.com>
2026-03-12 14:09:39 -0400

    Fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,12 @@
-    <footer>
-      <div class="d-flex justify-content-center gap-3">
-        <h5>&copy; 2025 Lab 5</h5>
-      </div>
-    </footer>
-    <script src="/Lab7/scss/js/bootstrap.bundle.min.js"></script>
-    <script src="/Lab7/scss/js/darkmodetoggle.js"></script>
-    <script src="/Lab7/scss/js/script.js"></script>
-  </body>
+<footer>
+  <div class="d-flex justify-content-center gap-3">
+    <h5>&copy; 2025 Lab 7</h5>
+  </div>
+</footer>
+
+<script src="/Lab7/scss/js/bootstrap.bundle.min.js"></script>
+<script src="/Lab7/scss/js/darkmodetoggle.js"></script>
+<script src="/Lab7/scss/js/script.js"></script>
+
+</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,54 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>My Pets</title>
-    <link rel="stylesheet" href="/Lab7/scss/css/bootstrap.css" />
-    <link rel="stylesheet" href="/Lab7/scss/css/style.css" />
-  </head>
-  <body>
-    <header class="mb-5">
-      <div class="container mt-5">
-        <div class="d-flex justify-content-end">
-          <div class="btn-group">
-            <button class="btn btn-secondary btn-sm" data-bs-theme-value="dark">
-              Dark
-            </button>
-          </div>
-          <div class="btn-group">
-            <button class="btn btn-light btn-sm" data-bs-theme-value="light">
-              Light
-            </button>
-          </div>
-        </div>
-        <h1 class="mb-4">Animals</h1>
-        <div class="row">
-          <div class="col-md-4">
-            <input
-              type="text"
-              class="form-control form-control-lg"
-              placeholder="Search Animals"
-            />
-          </div>
-          <div
-            class="col-md-8 user-menu d-flex justify-content-end align-items-center"
-          >
-            <ul class="nav">
-              <li class="nav-itm">
-                <a href="#" class="nav-link">Adaptations</a>
-              </li>
-              <li class="nav-itm">
-                <a href="/Lab7/php/behavior.php" class="nav-link text-bg-primary rounded-3">Behavior</a>
-              </li>
-              <li class="nav-itm">
-                <a href="#" class="nav-link">Conservation</a>
-              </li>
-              <li class="nav-itm">
-                <a href="#" class="nav-link">Diversity</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Animals</title>
+
+  <link rel="stylesheet" href="/Lab7/scss/css/bootstrap.css" />
+  <link rel="stylesheet" href="/Lab7/scss/css/style.css" />
+</head>
+
+<body>
+<header class="mb-5">
+  <div class="container mt-5">
+
+    <div class="d-flex justify-content-end">
+      <div class="btn-group">
+        <button class="btn btn-secondary btn-sm" data-bs-theme-value="dark">
+          Dark
+        </button>
       </div>
-    </header>
+
+      <div class="btn-group">
+        <button class="btn btn-light btn-sm" data-bs-theme-value="light">
+          Light
+        </button>
+      </div>
+    </div>
+
+    <h1 class="mb-4">Animals</h1>
+
+    <div class="row">
+
+      <div class="col-md-4">
+        <input
+          type="text"
+          class="form-control form-control-lg"
+          placeholder="Search Animals"
+        />
+      </div>
+
+      <div class="col-md-8 user-menu d-flex justify-content-end align-items-center">
+        <ul class="nav">
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">Adaptations</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="/Lab7/php/behavior.php" class="nav-link text-bg-primary rounded-3">
+              Behavior
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">Conservation</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">Diversity</a>
+          </li>
+
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</header>
