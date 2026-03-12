ef451e9b26d7f592e598c9b22219501e3c5266a1
Kaushal Basnet <kaushalbasnet988@gmail.com>
2026-03-12 13:58:10 -0400

    Restructured Project for Lab 7

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <div class="d-flex gap-3">
+            <img
+              src="/Lab7/scss/pics/penguins.jpg"
+              alt="Penguins"
+              class="img-fluid rounded-circle picture-size"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <div class="d-flex gap-3">
+            <img
+              src="/Lab7/scss/pics/puppies.webp"
+              alt="Puppies"
+              class="img-fluid rounded-circle picture-size"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <div class="d-flex gap-3">
+            <img
+              src="/Lab7/scss/pics/Cat.jpg"
+              alt="Cat"
+              class="img-fluid rounded-circle picture-size"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php include 'includes/footer.php'; ?>
---
+++ php/behavior.php
@@ -0,0 +1,22 @@
+<?php include '../includes/header.php'; ?>
+
+<main>
+  <div class="container">
+    <h2 class="mb-4">Animal Behavior</h2>
+    <p>
+      Animal behavior is the way animals act and respond to their surroundings.
+      Behavior helps animals find food, stay safe, communicate, and survive in nature.
+    </p>
+
+    <div class="mt-4">
+      <h4>Examples of animal behavior</h4>
+      <ul>
+        <li>Penguins stay close together to keep warm.</li>
+        <li>Puppies play to learn social and survival skills.</li>
+        <li>Cats observe quietly before reacting to danger.</li>
+      </ul>
+    </div>
+  </div>
+</main>
+
+<?php include '../includes/footer.php'; ?>
